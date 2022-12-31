8aa072b9b436f0ea36a5317438cc35592617ca3a
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2022-12-31 08:50:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://db:db@cluster0.dbyzuki.mongodb.net/";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    const db = client.db("reduxBlog");
+    const postCollection = db.collection("posts");
+
+    app.get("/posts", async (req, res) => {
+      const cursor = postCollection.find({});
+      const post = await cursor.toArray();
+
+      res.send({ status: true, data: post });
+    });
+
+    app.post("/post", async (req, res) => {
+      const post = req.body;
+
+      const result = await postCollection.insertOne(post);
+
+      res.send(result);
+    });
+
+    app.delete("/post/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const result = await postCollection.deleteOne({ _id: ObjectId(id) });
+      res.send(result);
+    });
+  } finally {
+  }
+};
+
+run().catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
